241837dddf78ffe06152e722b78422886604979c
hxppythxughts69 <axel-ekman-hakansson@hotmail.com>
2022-09-01 13:52:43 +0200

    consoleSetup metod more user input begining of while loop

--- NmrGG/src/Game.cpp
+++ NmrGG/src/Game.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <iostream>
 
+
 Game::Game() {
 	std::cout << "How many guesses do you want: ";
 	std::cin >> guesses;
@@ -12,9 +13,19 @@ Game::Game() {
 void Game::Run(){
 	std::srand(time(0));
 	int hiddenNmr = std::rand() % nmr;
+	int userGuess;
+
+	while (guesses > 0){
+		ConsoleSetup();
+		std::cout << "Your guess: ";
+		std::cin >> userGuess;
+
+		std::cin.get();
+	}
 
 }
 
 void Game::ConsoleSetup() {
-
+	std::cout << "\x1B[2J\x1B[H";
+	std::cout << "Guesses left: " << guesses << "\n" << "Rounds won: " << wins << "\n";
 }
