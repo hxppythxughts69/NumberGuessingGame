210d8042a33f544a7a1be5a576ffdf96cd0b612a
hxppythxughts69 <axel-ekman-hakansson@hotmail.com>
2022-09-01 14:23:26 +0200

    prototype done i think

--- NmrGG/src/Game.cpp
+++ NmrGG/src/Game.cpp
@@ -1,31 +1,64 @@
 #include "Game.h"
 #include <random>
 #include <iostream>
+#include <string>
 
 
 Game::Game() {
 	std::cout << "How many guesses do you want: ";
 	std::cin >> guesses;
+	uGuesses = guesses;
 	std::cout << "The hidden number shall be between 0 and: ";
 	std::cin >> nmr;
 }
 
 void Game::Run(){
-	std::srand(time(0));
-	int hiddenNmr = std::rand() % nmr;
-	int userGuess;
+	int hiddenNmr = GameReset();
+	int userGuess = 0;
+	std::string playAgain = "";
 
-	while (guesses > 0){
-		ConsoleSetup();
+	while (uGuesses > 0){
+		std::cout << hiddenNmr << "\n";
 		std::cout << "Your guess: ";
 		std::cin >> userGuess;
 
-		std::cin.get();
+		if (userGuess == hiddenNmr) {
+			wins++;
+			std::cout << "Play again? y/n: ";
+			std::cin >> playAgain;
+			if (playAgain == "y") {
+				hiddenNmr = GameReset();
+			}
+			else
+			{
+				exit(0);
+			}
+			
+		}
+		else if (userGuess < hiddenNmr) {
+			ConsoleSetup();
+			std::cout << "Higher than that!\n";
+			uGuesses -= 1;
+		}
+		else if (userGuess > hiddenNmr) {
+			ConsoleSetup();
+			std::cout << "Lower than that!\n";
+			uGuesses -= 1;
+		}
 	}
+
 
 }
 
 void Game::ConsoleSetup() {
 	std::cout << "\x1B[2J\x1B[H";
-	std::cout << "Guesses left: " << guesses << "\n" << "Rounds won: " << wins << "\n";
+	std::cout << "Guesses left: " << uGuesses << "\n" << "Rounds won: " << wins << "\n";
+}
+int Game::GameReset() {
+	std::srand(time(0));
+	int hiddenNmr = std::rand() % nmr;
+	uGuesses = guesses;
+	ConsoleSetup();
+
+	return hiddenNmr;
 }
--- NmrGG/src/Game.h
+++ NmrGG/src/Game.h
@@ -7,9 +7,11 @@ public:
 
 	void ConsoleSetup();
 	void Run();
+	int GameReset();
 
 private:
 	int guesses = 0;
 	int wins = 0;
 	int nmr = 0;
+	int uGuesses = 0;
 };
